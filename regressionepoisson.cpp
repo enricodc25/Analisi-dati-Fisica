0e8a819d1effc8385e3eb424d09d2ccc25d61e72
enricodc <121957581+matfis2@users.noreply.github.com>
2025-04-19 14:41:20 +0200

    Update regressionepoisson.cpp

--- regressionepoisson.cpp
+++ regressionepoisson.cpp
@@ -12,8 +12,8 @@ double DeviazioneStandardPosteriori(double a, double b, vector<double> x,vector<
 double CoefficienteCorrelazione(vector<double> x, vector<double> y);
 double chiquadroLineare(vector<double> x, vector <double> y, vector<double> sigmay, double a, double b);
 // double PValueChiquadro(double chiQuadrato, int gradiLiberta); //ha bisogno dell'iinstallazione della libreria Boost nel terminale in alternativa si usa l'approssimazione funzione gamma
-double p_value_chi_quadrato(double x, int k);
-double gamma_incompleta(double s, double z);
+// double p_value_chi_quadrato(double x, int k);
+// double gamma_incompleta(double s, double z);
 
 int main() {
 	string filename;
@@ -123,29 +123,29 @@ double chiquadroLineare(vector<double> x, vector<double> y, vector<double> sigma
 
 
 //------------------------APPROSSIMAZIONE p-value  PER N° gradi di libertà elevati------------------------------------//
-// Funzione Gamma incompleta inferiore
-double gamma_incompleta(double s, double z) {
-    const int max_iter = 100;
-    const double epsilon = 1e-6;
+// // Funzione Gamma incompleta inferiore
+// double gamma_incompleta(double s, double z) {
+//     const int max_iter = 100;
+//     const double epsilon = 1e-6;
 
-    double sum = 1.0 / s;
-    double term = sum;
-    for (int n = 1; n < max_iter; ++n) {
-        term *= z / (s + n);
-        sum += term;
-        if (std::fabs(term) < epsilon * sum) break;
-    }
-    return sum * std::exp(-z + s * std::log(z) - std::lgamma(s));
-}
+//     double sum = 1.0 / s;
+//     double term = sum;
+//     for (int n = 1; n < max_iter; ++n) {
+//         term *= z / (s + n);
+//         sum += term;
+//         if (std::fabs(term) < epsilon * sum) break;
+//     }
+//     return sum * std::exp(-z + s * std::log(z) - std::lgamma(s));
+// }
 
-// Calcolo del p-value CON L'APPOSSIMAZIONE DELLA FUNZIONE GAMMA
-double p_value_chi_quadrato(double x, int k) {
-    double s = k / 2.0;
-    double z = x / 2.0;
-    double gamma_incomp = gamma_incompleta(s, z);
-    double gamma_completa = std::tgamma(s);
-    return 1.0 - gamma_incomp / gamma_completa;
-}
+// // Calcolo del p-value CON L'APPOSSIMAZIONE DELLA FUNZIONE GAMMA
+// double p_value_chi_quadrato(double x, int k) {
+//     double s = k / 2.0;
+//     double z = x / 2.0;
+//     double gamma_incomp = gamma_incompleta(s, z);
+//     double gamma_completa = std::tgamma(s);
+//     return 1.0 - gamma_incomp / gamma_completa;
+// }
 
 //____________________________________________________________________________________________________________________________________//
 
@@ -205,6 +205,5 @@ void interpolazionePoisson(vector<double> x,vector<double> y, int cov){
     cout<<"-------------------------TEST STATISTICI----------------------"<<endl;
     cout <<"coefficiente di correlazione r = "<<CoefficienteCorrelazione(x,y)<<endl;
     cout<<"Il X^2 con "<<(x.size()-2)<<" gradi di liberta' X^2= "<<chiquadroLineare(x,y,sigmay,a,b)<<endl;
-    cout<<"Con un p-value= "<<p_value_chi_quadrato(chiquadroLineare(x,y,sigmay,a,b), (x.size()-2))<<endl<<endl;
     
 }
